4278f6baec1ecb15d4a35b8cdec8795f772c944a
mohammed sikder <sikderismail@gmail.com>
2021-01-09 17:02:34 -0500

    separated priority

--- src/components/message-list.js
+++ src/components/message-list.js
@@ -28,11 +28,11 @@ class MessageList extends React.PureComponent {
         message,
       ],
     }, () => {
-      // Included to support initial direction. Please remove upon completion
-      console.log(messages)
+      // Included to support initial direction. Please remove upon completion    
     })
   }
 
+ 
   handleClick = () => {
     const isApiStarted = this.api.isStarted()
     if (isApiStarted) {
@@ -45,14 +45,47 @@ class MessageList extends React.PureComponent {
 
   render() {
     const isApiStarted = this.api.isStarted()
+    const messages = this.state.messages
     return (
       <div>
         <Button
           variant="contained"
           onClick={this.handleClick}
-        >
+        >     
           {isApiStarted ? 'Stop Messages' : 'Start Messages'}
         </Button>
+
+        <div>
+        <p>{JSON.stringify(messages)}</p>
+        {
+            messages.map(message=>{
+              return <div>
+                { 
+                message.priority===1?
+                <div style={{textAlign: 'left'}}>
+                    <p>{message.message}</p>
+                   <p>{message.priority}</p>
+                </div> 
+                 :
+                 message.priority===2?
+                 <div style={{textAlign: 'center'}}>
+                      <p>{message.message}</p>
+                    <p>{message.priority}</p>
+                  </div> :
+                   <div style={{textAlign: 'right'}}>
+                      <p>{message.message}</p>
+                      <p>{message.priority}</p>
+                   </div>
+              }
+                </div>
+             
+             
+            }
+              
+            )
+        }
+           
+        </div>
       </div>
     )
   }
